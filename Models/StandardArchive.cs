da1056cf118fcafeb570a44aef165953cae7dbf1
DJ <dj.kunkel@ingentek.com>
2022-05-20 22:29:27 -0500

    intial commit

---
+++ Models/StandardArchive.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace RetroRen.Models
+{
+    [PageType(Title = "Standard archive", IsArchive = true)]
+    public class StandardArchive : Page<StandardArchive>
+    {
+    }
+}
---
+++ Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace RetroRen.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
